9266e23ca900ea9e59b1488a78b256de30310803
keremgorur <keyboard1099@gmail.com>
2020-02-13 12:34:12 +0300

    Add project files.

---
+++ WCA_Kerem/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace WCA_Kerem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           
+            long sayi7 = 324234;
+Console.WriteLine("Hello World!");
+
+            byte sayi1 = 56;
+
+            sbyte sayi2 = 12;
+
+            short sayi3 = 134;
+
+            ushort sayi4 = 123;
+
+            int sayi5 = 324;
+            
+            uint sayi6 = 21432432;
+            ulong sayi8 = 23424;
+
+            char karakter = 'A';
+
+
+
+            string metin = "Bilge Adam Beşiktaş Yazılım Dersleri";
+
+
+            
+            string tekkarakter = "A";
+
+            Console.WriteLine(metin);
+
+            string tabbosluk = "Bilge\tAdam";
+            Console.WriteLine(tabbosluk);
+
+            string deneme5 = "Bilge Adam Beşiktaş \"Yazılım\" Dersleri";
+
+            Console.WriteLine(deneme5);
+
+            bool sonuc = false;
+            bool result = 10 > 2;
+            bool result2 = 10 < 2;
+
+            Console.WriteLine(result);
+            Console.WriteLine(result2);
+
+
+
+
+
+
+
+        }
+    }
+}
